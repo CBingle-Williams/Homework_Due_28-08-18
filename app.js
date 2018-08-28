52265c0954fe240091477368180eac008617f312
CBingle-Williams <cbingle-williams@spartaglobal.com>
2018-08-28 19:29:16 +0100

    Added prime number function v1.0

---
+++ app.js
@@ -0,0 +1,18 @@
+function isPrime(n) {
+    if (n < 2) {
+        return false;
+    }
+    var q = Math.floor(Math.sqrt(n));
+    for (var i = 2; i <= q; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+for (var i = 0; i < 177; i++) {
+    if (isPrime(i)) {
+        console.log(i);
+    }
+}
